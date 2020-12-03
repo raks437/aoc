7f28bdcd61d76d4705139c7060521d231febe833
Rakesh Sharma <raks437@gmail.com>
2020-12-03 15:24:50 +0100

    added day4 template

---
+++ py/2020/day04/main.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+def main():
+    p1 = 0
+    p2 = 0
+    try:
+        with open('input.txt', 'r') as fin:
+            d = fin.read().splitlines()
+    finally:
+        fin.close()
+        print(f'Part 1: {p1}')
+        print(f'Part 2: {p2}')
+
+
+if __name__ == '__main__':
+    main()
