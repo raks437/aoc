018db06d996982b2490b9b716f95f19f4a409553
Rakesh Sharma <raks437@gmail.com>
2020-12-07 01:10:39 +0530

    added day 6

--- py/2020/day06/main.py
+++ py/2020/day06/main.py
@@ -5,12 +5,38 @@ def main():
     try:
         with open('input.txt', 'r') as fin:
             data = fin.read().splitlines()
+            cf = CustomsForms()
             for line in data:
-                print(line)
+                if line == '':
+                    p1 = p1 + cf.get_count()
+                    p2 = p2 + cf.get_count_v2()
+                    cf = CustomsForms()
+                else:
+                    cf.read(line)
     finally:
         fin.close()
         print(f'Part 1: {p1}')
         print(f'Part 2: {p2}')
+
+
+class CustomsForms:
+    def __init__(self):
+        self.all_entry = []
+
+    def read(self, form_data):
+        self.all_entry.append(form_data)
+
+    def get_count(self):
+        x = ''
+        x = x.join(self.all_entry)
+        uniques = list(set(list(x)))
+        return len(uniques)
+
+    def get_count_v2(self):
+        l = self.all_entry
+        sets = [set(x) for x in l]
+        n = sets[0].intersection(*sets[1:])
+        return len(n)
 
 
 if __name__ == '__main__':
